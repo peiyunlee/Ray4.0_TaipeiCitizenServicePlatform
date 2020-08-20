a5cdfbc08f619f40f427efdd324d4b8bd4b55bd9
PEI-YUN LEE <may1092200258@gmail.com>
2020-08-20 18:56:08 +0800

    Updates

---
+++ precache-manifest.5e06934a4507346596e7c46a7897a105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3edbdbda4bb4b518e9b25ab8b5da751d",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/index.html"
+  },
+  {
+    "revision": "e7bd86c03b1a76fba1a4",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "195435d76e511209953c",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/2.3781522f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/2.3781522f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7bd86c03b1a76fba1a4",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/main.5d6ab2db.chunk.js"
+  },
+  {
+    "revision": "a32550e41a29f237d96c",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/runtime-main.e73960f0.js"
+  }
+]);
