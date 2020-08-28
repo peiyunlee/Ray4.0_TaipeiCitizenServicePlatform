e6e211ca1b46cead0578c1f461528432d6ae73f8
PEI-YUN LEE <may1092200258@gmail.com>
2020-08-28 11:15:09 +0800

    Updates

--- precache-manifest.d1456b8bc7472b88c5637e8eb362b7a5.js
+++ precache-manifest.d1456b8bc7472b88c5637e8eb362b7a5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd5fd84da0862d254a4a9aeb00fe20a5",
+    "revision": "d1d8383cc0f6e13dd271dba7afaf9ff3",
     "url": "/Ray4.0_TaipeiCitizenServicePlatform/index.html"
   },
   {
-    "revision": "ccdb5d6ec29131ebc53a",
-    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/css/main.0af29e81.chunk.css"
+    "revision": "24bb7575715c5ee94d60",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/css/main.f85c681f.chunk.css"
   },
   {
-    "revision": "65c898fab7e7b98d6b4d",
-    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/2.0eedfb1d.chunk.js"
+    "revision": "5b8814238fcf9293a90a",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/2.199d14e6.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/2.0eedfb1d.chunk.js.LICENSE.txt"
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/2.199d14e6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ccdb5d6ec29131ebc53a",
-    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/main.a7c69e2c.chunk.js"
+    "revision": "24bb7575715c5ee94d60",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/main.c55bb451.chunk.js"
   },
   {
     "revision": "a32550e41a29f237d96c",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "6d8f99eee78f55335dbc04979e59ef84",
     "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/media/101.6d8f99ee.jpg"
+  },
+  {
+    "revision": "9a87bf7203f96a8795a8dc0d359e5b70",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/media/banner.9a87bf72.png"
   },
   {
     "revision": "904d64fa70984f6d19422419fec5c53a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Ray4.0_TaipeiCitizenServicePlatform/precache-manifest.121ee1a3ab765dcb8903a0795457b292.js"
+  "/Ray4.0_TaipeiCitizenServicePlatform/precache-manifest.d1456b8bc7472b88c5637e8eb362b7a5.js"
 );
 
 self.addEventListener('message', (event) => {
