6cce242c4b60ecf1756235bad5b3733359ecfb79
PEI-YUN LEE <may1092200258@gmail.com>
2020-08-28 11:13:48 +0800

    home

--- src/component/Home/sortcard.js
+++ src/component/Home/sortcard.js
@@ -12,6 +12,7 @@ class SortCard extends React.Component {
     if (listtype === "theme") {
       return (
         <Link className="card card-theme" to={"/caselist/" + listtype + "/" + list.name}>
+          <img className="card-theme-icon" src="" alt="" />
           <h3>{list.name}</h3>
           <div className="card-line"></div>
           <p>{list.info}</p>
