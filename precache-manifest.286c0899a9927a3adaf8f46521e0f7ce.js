6142c152597c7a7f859f185a31907d8240fbcc9e
Peiyun Lee <may1092200258@gmail.com>
2022-01-11 18:32:14 +0800

    Updates

---
+++ precache-manifest.286c0899a9927a3adaf8f46521e0f7ce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3902ea88433faef6172232f250bcafc1",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/index.html"
+  },
+  {
+    "revision": "f287f1f6062ad9b7e636",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/css/main.33da5ac1.chunk.css"
+  },
+  {
+    "revision": "3b147120abc79198f53e",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/2.f2c4fb4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/2.f2c4fb4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f287f1f6062ad9b7e636",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/main.7eef2028.chunk.js"
+  },
+  {
+    "revision": "a32550e41a29f237d96c",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/js/runtime-main.e73960f0.js"
+  },
+  {
+    "revision": "6d8f99eee78f55335dbc04979e59ef84",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/media/101.6d8f99ee.jpg"
+  },
+  {
+    "revision": "f9b8be89ef1bce32e7ebcb12356694cb",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/media/Frame.f9b8be89.png"
+  },
+  {
+    "revision": "904d64fa70984f6d19422419fec5c53a",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/media/index.904d64fa.less"
+  },
+  {
+    "revision": "7d2b719c0b303c01be4ff98612ef36f9",
+    "url": "/Ray4.0_TaipeiCitizenServicePlatform/static/media/search.7d2b719c.svg"
+  }
+]);
